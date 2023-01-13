53ef79e040c14c4d91be1049377c81f84109e10a
PrashantJain <Prashant.Jain@everymedia.com>
2023-01-13 14:03:04 +0530

    Team page id added

---
+++ wp-content/themes/cicd/template-parts/tpl-team.php
@@ -0,0 +1,11 @@
+<?
+/* 
+* Template Name: Team
+*/
+get_header();
+?>
+    <h2>Team page Template</h2>
+    <h6>to understand the CI/CD Pipeline setup</h6>
+    <h6>to understand the CI/CD Pipeline setup</h6>
+<?php 
+get_footer();
