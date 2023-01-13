85119e523728dca3c8e14fe5c165b53973a3e0f9
PrashantJain <Prashant.Jain@everymedia.com>
2023-01-13 17:53:23 +0530

    New file is added

---
+++ wp-content/themes/cicd/template-parts/tpl-pricing.php
@@ -0,0 +1,12 @@
+<?
+/* 
+* Template Name: Pricing
+*/
+get_header();
+?>
+    <h2>Pricing page Template</h2>
+    <h6>another value is added</h6>
+    <h6>another value is added</h6>
+    <h6>another value is added</h6>
+<?php 
+get_footer();
