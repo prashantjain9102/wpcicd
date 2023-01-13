1673099fe5ce863fe1b4af321319b3d5f4733618
PrashantJain <Prashant.Jain@everymedia.com>
2023-01-13 13:10:37 +0530

    Home page template is updated

--- wp-content/themes/cicd/template-parts/tpl-home.php
+++ wp-content/themes/cicd/template-parts/tpl-home.php
@@ -5,5 +5,9 @@
 get_header();
 ?>
     <h2>Home page Template</h2>
+    <h3>Newly Created</h3>
+    <h4>this is for test purpose</h4>
+    <h5>to check the workflow series</h5>
+    <h6>to understand the CI/CD Pipeline setup</h6>
 <?php 
 get_footer();
