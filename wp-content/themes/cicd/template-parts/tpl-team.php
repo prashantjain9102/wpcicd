99445b54773332bddfcbbb49e7e944f960f2e958
PrashantJain <Prashant.Jain@everymedia.com>
2023-01-13 18:00:23 +0530

    file is updated

--- wp-content/themes/cicd/template-parts/tpl-team.php
+++ wp-content/themes/cicd/template-parts/tpl-team.php
@@ -5,10 +5,6 @@
 get_header();
 ?>
     <h2>Team page Template</h2>
-    <h6>to understand the CI/CD Pipeline setup</h6>
-    <h6>to understand the CI/CD Pipeline setup</h6>
-    <h4>New line is updated to check the working</h4>
-    <h4>New line is updated to check the working</h4>
     <h6>New line is updated to check the working....</h6>
     <h6>..........New line is updated to check the working....</h6>
     <h6>..........New line is updated to check the working....</h6>
