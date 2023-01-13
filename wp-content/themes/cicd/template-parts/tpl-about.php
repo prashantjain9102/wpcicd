e4066c749eee07488f8afbc5213980efa9225c92
PrashantJain <Prashant.Jain@everymedia.com>
2023-01-13 13:58:55 +0530

    New file is added

---
+++ wp-content/themes/cicd/template-parts/tpl-about.php
@@ -0,0 +1,15 @@
+<?
+/* 
+* Template Name: About
+*/
+get_header();
+?>
+    <h2>About page Template</h2>
+    <h3>Newly Created</h3>
+    <h4>this is for test purpose</h4>
+    <h5>to check the workflow series</h5>
+    <h6>to understand the CI/CD Pipeline setup</h6>
+    <h6>to understand the CI/CD Pipeline setup</h6>
+    <h6>to understand the CI/CD Pipeline setup</h6>
+<?php 
+get_footer();
