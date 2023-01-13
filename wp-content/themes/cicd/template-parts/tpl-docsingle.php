27517c7c798de93a1405a1553f5af3bb3cdb9a35
PrashantJain <Prashant.Jain@everymedia.com>
2023-01-13 18:14:41 +0530

    Doctor file updated

--- wp-content/themes/cicd/template-parts/tpl-docsingle.php
+++ wp-content/themes/cicd/template-parts/tpl-docsingle.php
@@ -4,5 +4,15 @@
 */
 get_header(); ?>
     <h2>Doctors Detail Page</h2>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum maxime molestiae quis, aliquid nisi ea nihil nobis pariatur sequi quisquam incidunt id beatae minus magni odit. Rerum, molestiae sequi. </P>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum maxime molestiae quis, aliquid nisi ea nihil nobis pariatur sequi quisquam incidunt id beatae minus magni odit. Rerum, molestiae sequi. </P>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum maxime molestiae quis, aliquid nisi ea nihil nobis pariatur sequi quisquam incidunt id beatae minus magni odit. Rerum, molestiae sequi. </P>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum maxime molestiae quis, aliquid nisi ea nihil nobis pariatur sequi quisquam incidunt id beatae minus magni odit. Rerum, molestiae sequi. </P>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum maxime molestiae quis, aliquid nisi ea nihil nobis pariatur sequi quisquam incidunt id beatae minus magni odit. Rerum, molestiae sequi. </P>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum maxime molestiae quis, aliquid nisi ea nihil nobis pariatur sequi quisquam incidunt id beatae minus magni odit. Rerum, molestiae sequi. </P>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum maxime molestiae quis, aliquid nisi ea nihil nobis pariatur sequi quisquam incidunt id beatae minus magni odit. Rerum, molestiae sequi. </P>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum maxime molestiae quis, aliquid nisi ea nihil nobis pariatur sequi quisquam incidunt id beatae minus magni odit. Rerum, molestiae sequi. </P>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum maxime molestiae quis, aliquid nisi ea nihil nobis pariatur sequi quisquam incidunt id beatae minus magni odit. Rerum, molestiae sequi. </P>
+    <P> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum maxime molestiae quis, aliquid nisi ea nihil nobis pariatur sequi quisquam incidunt id beatae minus magni odit. Rerum, molestiae sequi. </P>
 <?php 
 get_footer();
